aa1a7a4fd41d0fe9f9c8f8e645238e1b7a1ed29b
David Hohn <hohn.david@gmail.com>
2014-10-08 00:13:18 +0200

    adding histo struct

--- makeHistos.C
+++ makeHistos.C
@@ -7,16 +7,25 @@ struct histo {
   Int_t nbins;
   Float_t xmin, xmax;
 
-  TString GetXtitle() {
+  histo(TString thisvar_name, Int_t thisnbins, Float_t thisxmin, Float_t thisxmax, TString thisunit=""):var_name(thisvar_name),xmin(thisxmin),xmax(thisxmax),unit(thisunit) {
+    GuessUnit();
+    MakeXtitle();
+    MakeYtitle();  
+  }
+
+  GuessUnit() {
+    //later
+  }
+
+  void MakeXtitle() {
     xtitle = var_name+" ["+unit+"]";
-    return xtitle;
-  };
-  TString GetYtitle() {
+  }
+  
+  void MakeYtitle() {
     Float_t eventsperbin = (xmax-xmin)/nbins;
     char s[10];
     sprintf(s,"%g",eventsperbin);
     ytitle = "/ "+s+" "+unit;
-    return ytitle;
   }
 };
 
@@ -27,6 +36,10 @@ int main(int argc, char* argv[])
   TFile *f_input = TFile::Open(f_name);
   TTree *t_input = (TTree*) f_input->Get("HWWTree");
 
+  int nhistos = 2;
+  histo histos[nhistos];
+  histos[0] = new histo("nJets_OR_T",10,0,10);
+  histos[1] = new histo("nJets_OR_T_MV1_70",10,0,10);
   
   
 }
